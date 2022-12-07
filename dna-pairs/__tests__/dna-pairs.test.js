7f9a9ffca27f628df6eed674e13ac78967433b87
BH-98 <bhume98@gmail.com>
2022-12-07 13:54:58 +0000

    Dna Pairs complete

--- dna-pairs/__tests__/dna-pairs.test.js
+++ dna-pairs/__tests__/dna-pairs.test.js
@@ -1,7 +1,16 @@
 const dnaPairs = require('../dna-pairs.js');
 
 describe('dnaPairs()', () => {
-  test('ADD YOUR TEST DESCRIPTION HERE', () => {
+  test('Returns an array ', () => {
     // add your test assertion(s) here
+    const input = ["G", "C"]
+    expect(Array.isArray(dnaPairs(input))).toBe(true)
   });
+  test('Returns an array containing all pairs', () => {
+    // add your test assertion(s) here
+    const input = ["G", "C"]
+    const expectedOutput = [["G", "C"], ["C", "G"]]
+    expect(dnaPairs(input)).toEqual(expectedOutput)
+  });
+  
 });
--- dna-pairs/dna-pairs.js
+++ dna-pairs/dna-pairs.js
@@ -1,6 +1,9 @@
 // Please do not change the name of this function
 function dnaPairs(dna) {
   // Write your code here
+  pairs = {G: "C", C: "G", A: "T", T: "A"}
+  const output = dna.map(base => [base, pairs[base]])
+  return output
 }
 
 module.exports = dnaPairs;
